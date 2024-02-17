37b916e94340391a3134cefa0a4ccddad6b1f5f0
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-17 21:55:18 +0500

    Add files via upload

---
+++ Exercise 23/conditionalTests.ts
@@ -0,0 +1,33 @@
+let car = 'subaru';
+let number = 42;
+let color = 'blue';
+//test 1
+console.log("Test 1: Is car equal to 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//test 2
+console.log("Test 2: Is number less than 50? I predict True.");
+console.log(number < 50);
+//test 3
+console.log("Test 3: Is color not equal to 'red'? I predict True.");
+console.log(color != 'red');
+//test 4
+console.log("Test 4: Is car not equal to 'toyota'? I predict True.");
+console.log(car != 'toyota');
+//test 5
+console.log("Test 5: Is number greater than or equal to 40? I predict True.");
+console.log(number >= 40);
+//test 6
+console.log("Test 6: Is color equal to 'green'? I predict False.");
+console.log(color == 'green');
+//test 7
+console.log("Test 7: Is car equal to 'subaru'? I predict False.");
+console.log(car == 'honda');
+//test 8
+console.log("Test 8: Is number less than or equal to 30? I predict False.");
+console.log(number <= 30);
+//test 9
+console.log("Test 9: Is color not equal to 'blue'? I predict False.");
+console.log(color != 'blue');
+//test 10
+console.log("Test 10: Is car not equal to 'subaru'? I predict False.");
+console.log(car != 'subaru');
