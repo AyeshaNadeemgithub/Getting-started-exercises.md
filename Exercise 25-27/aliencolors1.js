c209a449cbd6656d4550a4ee505784499d3b0dc8
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-17 23:09:43 +0500

    Add files via upload

---
+++ Exercise 25-27/aliencolors1.js
@@ -0,0 +1,17 @@
+"use strict";
+//if test passes
+let alien_color1 = 'green';
+if (alien_color1 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+}
+else {
+    console.log("No points earned. Keep playing!");
+}
+//if test fails
+let alien_color2 = 'red';
+if (alien_color2 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+}
+else {
+    console.log("No points earned. Keep playing!");
+}
---
+++ Exercise 25-27/aliencolors1.ts
@@ -0,0 +1,16 @@
+//if test passes
+let alien_color1: string = 'green';
+
+if (alien_color1 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+} else {
+    console.log("No points earned. Keep playing!");
+}
+//if test fails
+let alien_color2: string = 'red';
+
+if (alien_color2 === 'green') {
+    console.log("Congratulations! The player just earned 5 points.");
+} else {
+    console.log("No points earned. Keep playing!");
+}
