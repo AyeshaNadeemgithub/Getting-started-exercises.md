6ff337ec5417eb2cab5837182e3b7a15ff069a6e
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-17 21:21:08 +0500

    Add files via upload

---
+++ Exercise 21/object.js
@@ -0,0 +1,21 @@
+"use strict";
+// i am making objects having information about diff products
+let product1 = {
+    name: "Laptop",
+    category: "Electronics",
+    price: 999.99
+};
+let product2 = {
+    name: "Running Shoes",
+    category: "Footwear",
+    price: 79.99
+};
+let product3 = {
+    name: "Coffee Maker",
+    category: "Appliances",
+    price: 49.99
+};
+// print 
+console.log("Product 1:", product1);
+console.log("Product 2:", product2);
+console.log("Product 3:", product3);
---
+++ Exercise 21/object.ts
@@ -0,0 +1,24 @@
+// i am making objects having information about diff products
+let product1: { name: string; category: string; price: number } = {
+    name: "Laptop",
+    category: "Electronics",
+    price: 999.99
+  };
+  
+  let product2: { name: string; category: string; price: number } = {
+    name: "Running Shoes",
+    category: "Footwear",
+    price: 79.99
+  };
+  
+  let product3: { name: string; category: string; price: number } = {
+    name: "Coffee Maker",
+    category: "Appliances",
+    price: 49.99
+  };
+  
+  // print 
+  console.log("Product 1:", product1);
+  console.log("Product 2:", product2);
+  console.log("Product 3:", product3);
+  
