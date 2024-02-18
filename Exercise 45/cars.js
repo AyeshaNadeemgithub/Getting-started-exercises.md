be6bbb0c6e1cdabbdc6aee0edcb8779d1c2b786f
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-18 21:37:28 +0500

    Add files via upload

---
+++ Exercise 45/cars.js
@@ -0,0 +1,21 @@
+"use strict";
+// Function to store information about a car
+function createCar(manufacturer, modelName, ...options) {
+    // Create an object to store car information
+    const car = {
+        manufacturer: manufacturer,
+        modelName: modelName,
+    };
+    // Process additional keyword arguments
+    for (let i = 0; i < options.length; i += 2) {
+        const key = options[i];
+        const value = options[i + 1];
+        car[key] = value;
+    }
+    return car;
+}
+// Call the function with required and optional information
+const myCar = createCar("Toyota", "Camry", "color", "Blue", "year", 2022, "optionalFeature", "Sunroof");
+// Print the returned object to verify the information
+console.log("Car Information:");
+console.log(myCar);
