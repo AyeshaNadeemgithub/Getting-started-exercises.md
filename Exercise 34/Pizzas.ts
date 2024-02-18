df65a7bffcbbe71fdd00638ad5c7baae201bf7dc
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-18 11:42:45 +0500

    Add files via upload

---
+++ Exercise 34/Pizzas.ts
@@ -0,0 +1,16 @@
+let favoritePizzas: string[] = ['Pepperoni', 'double cheese', 'BBQ Chicken'];
+
+// Print the name of each pizza using a for loop
+console.log("Pizza Names:");
+for (let pizza of favoritePizzas) {
+    console.log(pizza);
+}
+
+// Print a sentence using the name of each pizza
+console.log("\nPizza Statements:");
+for (let pizza of favoritePizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+//  sentence outside the loop
+console.log("\nI really love pizza!");
