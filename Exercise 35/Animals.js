6b5dac854ea6189badab986a6c83883728e5880e
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-18 11:43:33 +0500

    Add files via upload

---
+++ Exercise 35/Animals.js
@@ -0,0 +1,17 @@
+"use strict";
+// List of animals with a common characteristic
+const animals = ["dog", "cat", "rabbit"];
+// Use a for loop to print the name of each animal
+console.log("Names of animals:");
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+//Use a for loop to print a statement about each animal
+console.log("\nStatements about each animal:");
+for (let i = 0; i < animals.length; i++) {
+    console.log(`A ${animals[i]} would make a great pet.`);
+}
+//  common characteristic
+console.log("\nAny of these animals would make a great pet!");
+//  common characteristic
+console.log("They are all mammals.");
