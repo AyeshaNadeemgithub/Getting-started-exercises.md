4db71f61501fadd69a3b6afeba0b0a4936445ef5
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-27 07:39:50 +0500

    Update aliencolors2.ts

---
+++ Exercise 25-27/aliencolors2.ts
@@ -0,0 +1,15 @@
+//true
+let alien_color1:string = 'green';
+
+if(alien_color1== 'green'){
+    console.log('you earned 5 points');
+}else
+    {console.log('you earned 10 points');}
+//false
+let alien_color:string = 'green';
+
+if(alien_color== 'red'){
+    console.log('you earned 5 points');
+}else
+    {console.log('you earned 10 points');}
+
