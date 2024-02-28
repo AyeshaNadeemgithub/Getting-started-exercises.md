c8bd5c983a4856a9ff5c3ad4c251ba83a420503a
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-28 21:15:12 +0500

    Update magicians.ts

---
+++ Exercise 41/magicians.ts
@@ -0,0 +1,12 @@
+function showMagicians(magicians: string[]): void {
+  magicians.forEach((magician) => {
+      console.log(magician);
+  });
+}
+
+// Array of magician's names
+const magicianNames: string[] = ["Merlin", "Houdini", "Gandalf", "David Copperfield"];
+
+// Call the function to display magician names
+showMagicians(magicianNames);
+
