90e667241313d3fa169b9f56620cbff86d40fcc3
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-28 23:19:41 +0500

    Update greatmagicians.js

--- Exercise 42/greatmagicians.js
+++ Exercise 42/greatmagicians.js
@@ -1,1 +1,26 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Function to show magicians
+function show_magicians(magicianArray) {
+    console.log("Magician Names:");
+    for (let i = 0; i < magicianArray.length; i++) {
+        console.log(magicianArray[i]);
+    }
+}
+// Function to make magicians great
+function make_great(magicianArray) {
+    const modifiedMagicians = [];
+    for (let i = 0; i < magicianArray.length; i++) {
+        modifiedMagicians.push(magicianArray[i] + " the Great");
+    }
+    return modifiedMagicians;
+}
+// Array of magician names
+const magicians = ["David Copperfield", "Houdini", "Penn Jillette", "Teller", "Derren Brown"];
+// Call make_great() to modify the array
+const greatMagicians = make_great([...magicians]);
+// Call show_magicians() with the modified array
+console.log("\nOriginal Magicians:");
+show_magicians(magicians);
+console.log("\nModified Magicians with the Great added:");
+show_magicians(greatMagicians);
