5f0b70124299342aac019349fabf9d93deacd88d
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-02-28 21:15:55 +0500

    Update magicians.js

--- Exercise 41/magicians.js
+++ Exercise 41/magicians.js
@@ -1,1 +1,11 @@
 "use strict";
+function showMagicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+// Array of magician's names
+var magicianNames = ["Merlin", "Houdini", "Gandalf", "David Copperfield"];
+// Call the function to display magician names
+showMagicians(magicianNames);
+
